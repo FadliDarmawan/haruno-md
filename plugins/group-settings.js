7e7ea1516ed51e4f7ff0cc1a69e240363094ba8c
Fadli Darmawan <fadli.darmawan03@gmail.com>
2022-04-14 04:33:58 +0700

    2.0.0

--- plugins/group-settings.js
+++ plugins/group-settings.js
@@ -1,4 +1,4 @@
-import { GroupSettingChange } from ('@adiwajshing/baileys')
+import { GroupSettingChange } from '@adiwajshing/baileys'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 	let isClose = {
 		'open': false,
@@ -15,7 +15,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 contoh:
 ${usedPrefix + command} tutup
 ${usedPrefix + command} buka
-	`.trim(), watermark, 'Buka', ',grup 1', 'Tutup', ',grup 0')
+	`.trim(), watermark, false, [['Buka', ',grup 1'], ['Tutup', ',grup 0']], m)
 		throw false
 	}
 	await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, isClose)
