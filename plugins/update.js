632cccda850544ffd2a1d538c061c4a38180c129
Fadli Darmawan <fadli.darmawan03@gmail.com>
2022-04-13 22:53:56 +0700

    2.0.0

--- plugins/update.js
+++ plugins/update.js
@@ -1,8 +1,9 @@
 import { execSync } from 'child_process'
+import fs from 'fs'
 let handler = async (m, { conn, text, isROwner }) => {
   if (global.conn.user.jid == conn.user.jid) {
     let stdout = execSync('git remote set-url origin https://github.com/FadliDarmawan/Hiroshi.git && git pull' + (isROwner && text ? ' ' + text : ''))
-    if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
+    if (isROwner) fs.readdirSync('plugins').map(v => global.reload('', v))
     m.reply(stdout.toString())
   }
 }
