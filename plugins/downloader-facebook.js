d926070cbde0050b9eeaace2f5f3dc881fb9d92f
Fadli Darmawan <fadli.darmawan03@gmail.com>
2022-04-14 17:20:52 +0700

    2.0.0

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,9 +1,9 @@
-import xa from 'xfarr-api'
-let handler = async(m, { conn, usedPrefix, args, command }) => {
+import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
     if(!args[0]) throw `Masukkan URL Facebook yang ingin di download!\n\nContoh: ${usedPrefix + command} https://www.facebook.com/100009307660961/videos/2850837675236460/`
-    let result = await xa.Facebook(args[0])
-    conn.sendFile(m.chat, result.medias[1].url, '', watermark, m)
-} 
+    const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
+    for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, watermark, m)
+}
 handler.help = ['facebbok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
