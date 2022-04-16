ccdb8f0d86f2bfc0caceb502ae258cb811b78449
Fadli Darmawan <fadli.darmawan03@gmail.com>
2022-04-16 13:48:24 +0700

    2.0.0

--- plugins/menu.js
+++ plugins/menu.js
@@ -440,7 +440,7 @@ function clockString(ms) {
   return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
 
-function ucapan() {
+async function ucapan() {
   const time = await moment.tz('Asia/Jakarta').format('HH')
   res = "Selamat dinihari"
   if (time >= 4) { 
