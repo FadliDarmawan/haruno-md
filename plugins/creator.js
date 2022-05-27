581a3f13d0218ed5d03b80673c55b8a7810af1fd
FadliDarmawan <fadli.darmawan03@gmail.com>
2022-05-27 20:28:59 +0700

    fix few bugs

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = async(m) => {
   let name = await conn.getName(owner[0])
-  let contact = await conn.sendContact(m.chat, owner[0], name, m)
+  let contact = await conn.sendContact(m.chat, owner[0] + '@s.whatsapp.net', name, m)
   await conn.reply(m.chat, 'Nomor *owner* itu bukan nomor bot.\nHarap pengertian nya nggak ngechat command bot ke nomer owner. Makasih.', contact)
 }
 handler.help = ['owner', 'creator']
