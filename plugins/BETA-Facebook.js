fec44451ce9336db4d3389c26ab77cdce17fe7c1
FadliDarmawan <fadli.darmawan03@gmail.com>
2022-06-19 15:55:41 +0700

    ...

--- plugins/BETA-Facebook.js
+++ plugins/BETA-Facebook.js
@@ -3,7 +3,7 @@
 import getDownloadUrl from 'facebook-video-downloader'
 let handler = async(m, { conn, usedPrefix, text, command }) => {
     if (!text) throw `Masukkan URL Facebook yang ingin di download!\n\nContoh: ${usedPrefix + command} https://www.facebook.com/100009307660961/videos/2850837675236460/`
-    let url = await getDownloadUrl(args[0])
+    let url = await getDownloadUrl(text)
     console.log(url)
 }
 handler.command = /^bfbdownload$/i
